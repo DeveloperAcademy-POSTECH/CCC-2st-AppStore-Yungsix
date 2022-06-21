1cdc2beed5cbfa9e1d3441bbaef31ee655ffe6ae
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-21 23:07:29 +0900

    first commit

---
+++ YungsixAppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YungsixAppStore
+//
+//  Created by 최윤석 on 2022/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YungsixAppStore/YungsixAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  YungsixAppStoreApp.swift
+//  YungsixAppStore
+//
+//  Created by 최윤석 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct YungsixAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
