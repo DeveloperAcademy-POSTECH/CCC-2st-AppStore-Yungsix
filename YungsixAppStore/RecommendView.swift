872ef70387d55277899c5ce4d4f7c8cb748cca3e
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-25 10:32:28 +0900

    Style: 이미지 추가

---
+++ YungsixAppStore/RecommendView.swift
@@ -0,0 +1,20 @@
+//
+//  RecommendView.swift
+//  YungsixAppStore
+//
+//  Created by 최윤석 on 2022/06/23.
+//
+
+import SwiftUI
+
+struct RecommendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecommendView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecommendView()
+    }
+}
